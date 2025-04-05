20c86aa67d271b466c64b73e3b6d32d4b756386b
Jose Maria Rapariz <josemariarapariz@gmail.com>
2025-04-05 13:51:33 -0500

    Add form page swap logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-function showFAQ(group, button){
+function showFAQ(group, button) {
 
     // Hide and show faq sections
     document.querySelectorAll('.faq-group').forEach(el => el.classList.add('d-none'));
@@ -7,4 +7,28 @@ function showFAQ(group, button){
     // Styles of active button
     document.querySelectorAll('.faq-tab-btn').forEach(btn => btn.classList.remove('fw-bold'))
     button.classList.add('fw-bold');
+}
+
+let currPage = 1;
+
+function nextPage() {
+    currPage++;
+    if(currPage >= 2){
+        document.querySelector('.btn-outline-secondary').classList.remove('d-none');
+    }
+    document.querySelectorAll('.form-page').forEach(fp => fp.classList.add('d-none'));
+    document.querySelector(`#form-page-${currPage}`).classList.remove('d-none');
+}
+
+function prevPage(btn) {
+    currPage--;
+    if(currPage < 1){
+        btn.classList.add('d-none');
+        return;
+    } 
+    document.querySelectorAll('.form-page').forEach(fp => fp.classList.add('d-none'));
+    document.querySelector(`#form-page-${currPage}`).classList.remove('d-none');
+    if(currPage === 1){
+        btn.classList.add('d-none');
+    }
 }
